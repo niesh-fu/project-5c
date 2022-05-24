c8305134ba069ec7b2636624bddfe22553389ea2
Zuber Khan <50729008+ZUBERKHAN034@users.noreply.github.com>
2022-05-24 13:38:31 +0530

    ADDED userModel

---
+++ src/models/userModel.js
@@ -0,0 +1,79 @@
+const mongoose = require('mongoose')
+const userSchema = new mongoose.Schema({
+
+
+    fname: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    lname: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    profileImage: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    phone: {
+        type: String,
+        required: true,
+        trim: true,
+        length: 10,
+        unique: true
+    },
+    password: {
+        type: String,
+        require: true,
+        trim: true
+    },
+
+    address: {
+        shipping: {
+            street: {
+                type: String,
+                required: true,
+                trim: true
+            },
+            city: {
+                type: String,
+                required: true,
+                trim: true
+            },
+            pincode: {
+                type: Number,
+                required: true,
+                max: 6
+            }
+        },
+        billing: {
+            street: {
+                type: String,
+                required: true,
+                trim: true
+            },
+            city: {
+                type: String,
+                required: true,
+                trim: true
+            },
+            pincode: {
+                type: Number,
+                required: true,
+                max: 6
+            }
+        }
+    }
+
+
+}, { timestamps: true })
+
+module.exports = mongoose.model('user', userSchema);
